bb25b26881f1c52bb2c4050852f052581dbded80
Abhishek Kumar <35186422+abhy-kumar@users.noreply.github.com>
2018-01-23 20:48:19 +0530

    Added Another Page.
    The code is now modular, verifiedly so.

--- Python/ui_primary.py
+++ Python/ui_primary.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
-LARGE_FONT = ("Segoe UI Light", 42)
+font_x = ("Segoe UI Light", 42)
 
 
 class rss_core(tk.Tk):
@@ -15,7 +15,7 @@ class rss_core(tk.Tk):
 
         self.frames = {}
 
-        for F in (StartPage, PageOne):
+        for F in (StartPage, PageOne, PageTwo):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
@@ -37,26 +37,34 @@ class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, bg='Grey')
-        label = tk.Label(self, text="Hi!", font=LARGE_FONT, bg='Grey')
+        label = tk.Label(self, text="Hi!", font=font_x, bg='Grey')
         label.pack(pady=10, padx=10)
-        entry1 = tk.Entry(self)
-        entry1.pack()
 
-        s = entry1.get()
-
-        button1 = tk.Button(self, text='Confirm', command=lambda: controller.show_frame(PageOne))
+        button1 = tk.Button(self, text='Page One', command=lambda: controller.show_frame(PageOne))
         button1.pack()
 
+        button1 = tk.Button(self, text='Page Two', command=lambda: controller.show_frame(PageTwo))
+        button1.pack()
 
 class PageOne(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Hi!, user", font=LARGE_FONT, bg='Grey')
+        label = tk.Label(self, text="Hi!, user", font=font_x, bg='Grey')
         label.pack(pady=10, padx=10)
 
-        button1 = tk.Button(self, text='Back', command=lambda: controller.show_frame(StartPage))
-
+        button1 = tk.Button(self, text='Home', command=lambda: controller.show_frame(StartPage))
         button1.pack()
+
+class PageTwo(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="This is PageTwo", font=font_x, bg='Grey')
+        label.pack(pady=10, padx=10)
+
+        button1 = tk.Button(self, text='Home', command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        
 
 app = rss_core()
 app.mainloop()
