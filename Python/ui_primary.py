53eb7f02ea63a0e4afb49cf3450f1833ef3434ae
Abhishek Kumar <35186422+abhy-kumar@users.noreply.github.com>
2018-01-23 21:05:41 +0530

    Styling

--- Python/ui_primary.py
+++ Python/ui_primary.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 font_x = ("Segoe UI Light", 42)
 
@@ -36,32 +37,32 @@ class rss_core(tk.Tk):
 class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent, bg='Grey')
-        label = tk.Label(self, text="Hi!", font=font_x, bg='Grey')
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Hi!", font=font_x)
         label.pack(pady=10, padx=10)
 
-        button1 = tk.Button(self, text='Page One', command=lambda: controller.show_frame(PageOne))
+        button1 = ttk.Button(self, text='Page One', command=lambda: controller.show_frame(PageOne))
         button1.pack()
 
-        button1 = tk.Button(self, text='Page Two', command=lambda: controller.show_frame(PageTwo))
+        button1 = ttk.Button(self, text='Page Two', command=lambda: controller.show_frame(PageTwo))
         button1.pack()
 
 class PageOne(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Hi!, user", font=font_x, bg='Grey')
+        label = tk.Label(self, text="Hi!, user", font=font_x)
         label.pack(pady=10, padx=10)
 
-        button1 = tk.Button(self, text='Home', command=lambda: controller.show_frame(StartPage))
+        button1 = ttk.Button(self, text='Home', command=lambda: controller.show_frame(StartPage))
         button1.pack()
 
 class PageTwo(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="This is PageTwo", font=font_x, bg='Grey')
+        label = tk.Label(self, text="This is PageTwo", font=font_x)
         label.pack(pady=10, padx=10)
 
-        button1 = tk.Button(self, text='Home', command=lambda: controller.show_frame(StartPage))
+        button1 = ttk.Button(self, text='Home', command=lambda: controller.show_frame(StartPage))
         button1.pack()
 
         
